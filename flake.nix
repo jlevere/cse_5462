37335e1dc9c14e97792bbf611e0d29b99bfd0c59
jLevere <71566629+jLevere@users.noreply.github.com>
2025-01-04 16:27:13 -0500

    setup nix env

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "A Nix-flake-based C development environment";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forEachSupportedSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import nixpkgs {inherit system;};
+        });
+  in {
+    devShells = forEachSupportedSystem ({pkgs}: {
+      default =
+        pkgs.mkShell.override
+        {
+          # Override stdenv in order to change compiler:
+          # stdenv = pkgs.clangStdenv;
+        }
+        {
+          packages = with pkgs;
+            [
+              cmake
+              make
+              codespell
+              doxygen
+              gtest
+              lcov
+              gcc
+              ctags
+              valgrind
+            ]
+            ++ (
+              if system == "aarch64-darwin"
+              then []
+              else [gdb]
+            );
+        };
+    });
+  };
+}
