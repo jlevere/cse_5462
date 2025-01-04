46ad6ceda8a80327675d56a7ca5769f9a30d57ea
jLevere <71566629+jLevere@users.noreply.github.com>
2025-01-04 16:38:34 -0500

    added typst and harper

--- flake.nix
+++ flake.nix
@@ -1,17 +1,27 @@
 {
-  description = "A Nix-flake-based C development environment";
+  description = "A Nix-flake-based development enviroment for CSE 5462";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    harper = {
+      url = "github:grantlemons/harper";
+      flake = false;
+    };
+  };
 
   outputs = {
     self,
     nixpkgs,
+    harper,
   }: let
     supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
     forEachSupportedSystem = f:
       nixpkgs.lib.genAttrs supportedSystems (system:
         f {
-          pkgs = import nixpkgs {inherit system;};
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          };
         });
   in {
     devShells = forEachSupportedSystem ({pkgs}: {
@@ -24,15 +34,22 @@
         {
           packages = with pkgs;
             [
-              cmake
-              make
               codespell
-              doxygen
-              gtest
-              lcov
               gcc
               ctags
               valgrind
+
+              typst
+              (pkgs.rustPlatform.buildRustPackage {
+                pname = "harper-ls";
+                version = "master";
+
+                src = harper;
+
+                cargoLock = null;
+                cargoBuild = true;
+                cargoHash = "sha256-2IRM7Ttaw30c99U39+YqL9GzCEpXfTrmBvdxD4FPbuM=";
+              })
             ]
             ++ (
               if system == "aarch64-darwin"
