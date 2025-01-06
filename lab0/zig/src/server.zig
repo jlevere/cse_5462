5b1665c2a367caf18eb226507904775c47791a31
jLevere <71566629+jLevere@users.noreply.github.com>
2025-01-06 16:57:22 -0500

    updated server code for better integration with client

--- lab0/zig/src/server.zig
+++ lab0/zig/src/server.zig
@@ -1,81 +1,11 @@
 const std = @import("std");
-
-const UDPSocket = struct {
-    const Self = @This();
-    socketfd: std.posix.socket_t,
-    addr: std.net.Address,
-
-    recvbuf: [65535]u8,
-
-    pub fn init(addr: std.net.Address) !Self {
-        return Self{
-            .socketfd = try std.posix.socket(
-                addr.any.family,
-                std.posix.SOCK.DGRAM,
-                std.posix.IPPROTO.UDP,
-            ),
-            .addr = addr,
-            .recvbuf = undefined,
-        };
-    }
-
-    pub fn deinit(self: Self) void {
-        std.posix.close(self.socketfd);
-    }
-
-    pub fn bind(self: Self) !void {
-        try std.posix.setsockopt(
-            self.socketfd,
-            std.posix.SOL.SOCKET,
-            std.posix.SO.REUSEADDR,
-            &std.mem.toBytes(@as(c_int, 1)),
-        );
-        try std.posix.bind(
-            self.socketfd,
-            &self.addr.any,
-            self.addr.getOsSockLen(),
-        );
-        std.debug.print("[+] socket bound\n", .{});
-    }
-
-    pub fn listen(self: *Self) !void {
-        std.debug.print("[+] listening on port {}\n", .{self.addr.getPort()});
-        while (true) {
-            var client_addr: std.net.Address = undefined;
-            var client_addr_len: std.posix.socklen_t = self.addr.getOsSockLen();
-
-            const bytes_recved = try std.posix.recvfrom(
-                self.socketfd,
-                &self.recvbuf,
-                0,
-                &client_addr.any,
-                &client_addr_len,
-            );
-
-            if (bytes_recved > 0) {
-                std.debug.print("[+] recv {} bytes from {} - '{s}'\n", .{ bytes_recved, client_addr, self.recvbuf[0..bytes_recved] });
-            }
-
-            const send_msg = "Welcome to CSE5462.";
-
-            _ = try std.posix.sendto(
-                self.socketfd,
-                send_msg,
-                0,
-                &client_addr.any,
-                client_addr_len,
-            );
-
-            std.debug.print("[+] {} connected\n", .{client_addr});
-        }
-    }
-};
+const UDPSocket = @import("sock.zig").UDPSocket;
 
 pub fn main() !void {
-    var socket = try UDPSocket.init(try std.net.Address.resolveIp("127.0.0.1", 8001));
+    var socket = try UDPSocket.init();
     defer socket.deinit();
 
-    try socket.bind();
+    try socket.bind(try std.net.Address.resolveIp("127.0.0.1", 8001));
 
     try socket.listen();
 }
