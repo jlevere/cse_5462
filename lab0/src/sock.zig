f9705375fa55c0268560a510b963c6ca797c9374
jLevere <71566629+jLevere@users.noreply.github.com>
2025-01-11 22:01:43 -0500

    updated sock and server to use callbacks and logging

--- lab0/src/sock.zig
+++ lab0/src/sock.zig
@@ -1,11 +1,20 @@
 const std = @import("std");
 
+const socket_log = std.log.scoped(.socket);
+
 pub const UDPSocket = struct {
     const Self = @This();
     socketfd: std.posix.socket_t,
     addr: std.net.Address,
 
     recvbuf: [65535]u8,
+    sendbuf: [65535]u8,
+
+    pub const MessageHandler = fn (
+        client_addr: std.net.Address,
+        recv_data: []const u8,
+        resp_buf: []u8,
+    ) ?usize;
 
     pub fn init() !Self {
         return Self{
@@ -16,6 +25,7 @@ pub const UDPSocket = struct {
             ),
             .addr = undefined,
             .recvbuf = undefined,
+            .sendbuf = undefined,
         };
     }
 
@@ -62,11 +72,12 @@ pub const UDPSocket = struct {
             &addr.any,
             addr.getOsSockLen(),
         );
-        std.debug.print("[+] socket bound\n", .{});
+
+        socket_log.info("socket bound '{}'", .{addr});
     }
 
-    pub fn listen(self: *Self) !void {
-        std.debug.print("[+] listening on port {}\n", .{self.addr.getPort()});
+    pub fn listen(self: *Self, handler: MessageHandler) !void {
+        socket_log.info("listening on port {}", .{self.addr.getPort()});
         while (true) {
             var client_addr: std.net.Address = undefined;
             var client_addr_len: std.posix.socklen_t = self.addr.getOsSockLen();
@@ -79,21 +90,25 @@ pub const UDPSocket = struct {
                 &client_addr_len,
             );
 
-            if (bytes_recved > 0) {
-                std.debug.print("[+] recv {} bytes from {} - '{s}'\n", .{ bytes_recved, client_addr, self.recvbuf[0..bytes_recved] });
+            if (bytes_recved == 0) {
+                continue;
             }
 
-            const send_msg = "Welcome to CSE5462.";
+            const recv_data = self.recvbuf[0..bytes_recved];
 
-            _ = try std.posix.sendto(
-                self.socketfd,
-                send_msg,
-                0,
-                &client_addr.any,
-                client_addr_len,
-            );
+            socket_log.debug("connection from: '{}' recv: '{s}'", .{ client_addr, recv_data });
 
-            std.debug.print("[+] {} connected\n", .{client_addr});
+            if (handler(client_addr, recv_data, &self.sendbuf)) |resp_len| {
+                _ = try std.posix.sendto(
+                    self.socketfd,
+                    self.sendbuf[0..resp_len],
+                    0,
+                    &client_addr.any,
+                    client_addr_len,
+                );
+
+                socket_log.debug("sent to '{}' data: '{s}'", .{ client_addr, self.sendbuf[0..resp_len] });
+            }
         }
     }
 };
